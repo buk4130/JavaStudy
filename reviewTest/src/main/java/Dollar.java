67e2d806f27e71a723bc0c271a91ecc5f52230ae
Seong ho Hong <buk4130@gmail.com>
2017-01-15 21:15:54 +0900

    Dollar 부작용 제거

--- reviewTest/src/main/java/Dollar.java
+++ reviewTest/src/main/java/Dollar.java
@@ -6,9 +6,10 @@ public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
-
+        this.amount = amount;
     }
 
-    public void times(int multiplier) {
+    public Dollar times(int multiplier) {
+        return new Dollar(amount*multiplier);
     }
 }

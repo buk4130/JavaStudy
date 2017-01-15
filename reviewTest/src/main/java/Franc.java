a117df64762d77d4f8f843edc9559acb496b1a14
Seong ho Hong <buk4130@gmail.com>
2017-01-15 22:02:25 +0900

    중복 문제 발생

---
+++ reviewTest/src/main/java/Franc.java
@@ -0,0 +1,21 @@
+/**
+ * Created by seonghohong on 2017. 1. 15..
+ */
+public class Franc {
+
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount*multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+
+        return amount == franc.amount;
+    }
+}

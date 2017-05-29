a33669895aa1b16d34e91e534296f0311373f461
buk4130 <buk4130@gmail.com>
2017-05-29 11:48:32 +0900

    success project_03

--- codeground/project_03/src/main/java/Main.java
+++ codeground/project_03/src/main/java/Main.java
@@ -1,51 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
  * Created by samsung on 2017-05-24.
  */
 public class Main {
-
     static int Answer;
-    private static int num;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws Exception	{
         Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
-        for (int test_case = 0; test_case < T; test_case++) {
+        for(int test_case = 0; test_case < T; test_case++) {
 
-            int num = getNum();
-            int[] pointList = getPointList(num);
+            int num = sc.nextInt();
+            ArrayList<Integer> pointList = new ArrayList<Integer>();
+            for(int i=0; i<num; i++) {
+                pointList.add(sc.nextInt());
+            }
 
-            Answer = solution(pointList);
+            Collections.sort(pointList);
+            int maxPoint = 0;
 
-            System.out.println("Case #" + (test_case + 1));
+            int pointSize = pointList.size();
+
+            for(int i=0; i<pointSize; i++) {
+                int tmp = pointList.get(i) + (pointSize-i);
+                if(tmp>maxPoint) maxPoint = tmp;
+            }
+
+            Answer = 0;
+
+            for(int i=0; i<pointSize; i++) {
+                if ((pointSize+pointList.get(i)) >= maxPoint) {
+                    Answer++;
+                }
+            }
+
+
+            System.out.println("Case #"+(test_case+1));
             System.out.println(Answer);
         }
-    }
-
-    private static int solution(int[] pointList) {
-
-
-        return num;
-    }
-
-    public static int getNum() {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-
-
-        return num;
-    }
-
-    public static int[] getPointList(int num) {
-        Scanner scanner = new Scanner(System.in);
-        int[] list = new int[num];
-
-        for (int i=0; i<num; i++) {
-            list[i] = scanner.nextInt();
-        }
-
-        return list;
     }
 }
